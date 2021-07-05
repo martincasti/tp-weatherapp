39e5f62e5f43481d643dc5569080623bc40b7297
martincasti <martin.castineira@davinci.edu.ar>
2021-07-05 18:44:04 -0300

    weatherApp

---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Weather = (props) => {
+  return (
+    <dv className="cointainer">
+      <div className="cards col-md-10 mt-5">
+        <h1>{props.city}</h1>
+        <h5 className="py-4">
+          <i className={`wi ${props.weatherIcon} display-1`}></i>
+        </h5>
+        {props.temp_celsius ? (
+          <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+        ) : null}
+
+        {/** show max and min temp */}
+        {minmaxTemp(props.temp_min, props.temp_max)}
+
+        <h4 className="py-3">{props.description}</h4>
+      </div>
+    </dv>
+  );
+};
+
+function minmaxTemp(min, max) {
+  if (min && max) {
+    return (
+      <h3>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{max}&deg;</span>
+      </h3>
+    );
+  }
+}
+
+export default Weather;
